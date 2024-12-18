7efbb896bb188885d2df88472bb707a9d4253e72
KotsiosDimis <konstandinosdimitriou@gmail.com>
2024-12-18 20:29:21 +0200

    testongv7

--- lib/users.php
+++ lib/users.php
@@ -63,7 +63,7 @@ function checkSession() {
     session_start();
 
     if (isset($_SESSION['user_id'])) {
-        echo json_encode(['loggedIn' => true, 'user_id' => $_SESSION['user_id']]);
+        echo json_encode(['loggedIn' => true, 'user_id' => $_SESSION['user_id'] , 'username' => $_SESSION['username']]);
     } else {
         echo json_encode(['loggedIn' => false]);
     }
