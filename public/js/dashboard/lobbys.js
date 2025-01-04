fb5a24a5743d47822a5a1213e9a4d768bd30409c
ashikkalis <stavrosash@gmail.com>
2025-01-04 15:38:19 +0200

    Changes in lobbys.js (6)

--- public/js/dashboard/lobbys.js
+++ public/js/dashboard/lobbys.js
@@ -101,6 +101,13 @@
             // });
 
             // ul.appendChild(li);
+
+            // Optional: Add functionality for the "Join" button
+            li.querySelector('button').addEventListener('click', () => {
+                window.location.href = `game.html?lobby_id=${lobbys.game_id}`;
+            });
+
+            ul.appendChild(li);
         });
                 
 
