461d696658673170f5762739075d6a7b30e90bcb
ashikkalis <stavrosash@gmail.com>
2025-01-09 11:35:27 +0200

    Join button in Lobby.js file (55)

--- public/js/dashboard/lobbys.js
+++ public/js/dashboard/lobbys.js
@@ -27,53 +27,53 @@
                 const li = document.createElement('li');
                 li.className = 'list-group-item d-flex justify-content-between align-items-center';
 
+            //     li.innerHTML = `
+            //     <span>
+            //         <strong>Lobby #${lobbys.game_id},</strong> - <strong>Player1(Host):</strong> ${lobbys.host}, 
+            //     </span>
+                
+            //     <button class="btn btn-primary btn-sm ready-btn1" style="display:none;" data-user-id="${lobbys.host}">Ready</button>
+
+            //     <span> 
+            //         <strong>Player2:</strong> ${lobbys.player2}, 
+            //     </span>
+
+            //     <button class="btn btn-primary btn-sm ready-btn2" style="display:none;" data-user-id="${lobbys.player2}">Ready</button>
+
+            //     <span>
+            //         <strong>Player3:</strong> ${lobbys.player3}, 
+            //     </span>
+
+            //     <button class="btn btn-primary btn-sm ready-btn3" style="display:none;" data-user-id="${lobbys.player3}">Ready</button>
+
+            //     <span>
+            //         <strong>Player4:</strong> ${lobbys.player4}, 
+            //     </span>
+
+            //     <button class="btn btn-primary btn-sm ready-btn4" style="display:none;" data-user-id="${lobbys.player4}">Ready</button>
+
+            //     <span>
+            //         <strong>Game Status:</strong> ${lobbys.game_status}
+            //     </span>
+
+            //     <button class="btn btn-primary btn-sm join-btn" data-lobby-id="${lobbys.game_id}">Join</button>
+            //     <button class="btn btn-primary btn-sm start-btn" style="display:none;" data-lobby-id="${lobbys.game_id}">Start</button>
+            //     `;
+
                 li.innerHTML = `
                 <span>
-                    <strong>Lobby #${lobbys.game_id},</strong> - <strong>Player1(Host):</strong> ${lobbys.host}, 
+                    <strong>Lobby #${lobbys.game_id},</strong> - <strong>Player1(Host):</strong> ${lobbys.host}, <strong>Player2:</strong> ${lobbys.player2}, <strong>Player3:</strong> ${lobbys.player3}, <strong>Player4:</strong> ${lobbys.player4}, <strong>Game Status:</strong> ${lobbys.game_status}
                 </span>
-                
-                <button class="btn btn-primary btn-sm ready-btn1" style="display:none;" data-user-id="${lobbys.host}">Ready</button>
-
-                <span> 
-                    <strong>Player2:</strong> ${lobbys.player2}, 
-                </span>
-
-                <button class="btn btn-primary btn-sm ready-btn2" style="display:none;" data-user-id="${lobbys.player2}">Ready</button>
-
-                <span>
-                    <strong>Player3:</strong> ${lobbys.player3}, 
-                </span>
-
-                <button class="btn btn-primary btn-sm ready-btn3" style="display:none;" data-user-id="${lobbys.player3}">Ready</button>
-
-                <span>
-                    <strong>Player4:</strong> ${lobbys.player4}, 
-                </span>
-
-                <button class="btn btn-primary btn-sm ready-btn4" style="display:none;" data-user-id="${lobbys.player4}">Ready</button>
-
-                <span>
-                    <strong>Game Status:</strong> ${lobbys.game_status}
-                </span>
-
                 <button class="btn btn-primary btn-sm join-btn" data-lobby-id="${lobbys.game_id}">Join</button>
+                <button class="btn btn-primary btn-sm ready-btn" style="display:none;" data-user-id="${lobbys.user_id}">Ready</button>
                 <button class="btn btn-primary btn-sm start-btn" style="display:none;" data-lobby-id="${lobbys.game_id}">Start</button>
                 `;
 
-                // li.innerHTML = `
-                // <span>
-                //     <strong>Lobby #${lobbys.game_id},</strong> - <strong>Player1(Host):</strong> ${lobbys.host}, <strong>Player2:</strong> ${lobbys.player2}, <strong>Player3:</strong> ${lobbys.player3}, <strong>Player4:</strong> ${lobbys.player4}, <strong>Game Status:</strong> ${lobbys.game_status}
-                // </span>
-                // <button class="btn btn-primary btn-sm join-btn" data-lobby-id="${lobbys.game_id}">Join</button>
-                // <button class="btn btn-primary btn-sm ready-btn" style="display:none;" data-user-id="${lobbys.user_id}">Ready</button>
-                // <button class="btn btn-primary btn-sm start-btn" style="display:none;" data-lobby-id="${lobbys.game_id}">Start</button>
-                // `;
-
                 const joinButton = li.querySelector('.join-btn');
-                const readyButton1 = li.querySelector('.ready-btn1');
-                const readyButton2 = li.querySelector('.ready-btn2');
-                const readyButton3 = li.querySelector('.ready-btn3');
-                const readyButton4 = li.querySelector('.ready-btn4');
+                const readyButton = li.querySelector('.ready-btn');
+                // const readyButton2 = li.querySelector('.ready-btn2');
+                // const readyButton3 = li.querySelector('.ready-btn3');
+                // const readyButton4 = li.querySelector('.ready-btn4');
                 const startButton = li.querySelector('.start-btn');
 
                 li.querySelector('.join-btn').addEventListener('click', async (event) => {
@@ -100,21 +100,21 @@
                             alert(result.message);
                             joinButton.disabled = true;
                             joinButton.style.display = 'none';
+                            readyButton.style.display = 'inline-block';
+                            readyButton.disabled = false;
 
-                            if(userId == userId){
-                                readyButton1.style.display = 'inline-block';
-                            }
-                            if (lobbys.player2 == userId) {
-                                readyButton2.style.display = 'inline-block';
-                            }
-                            if (lobbys.player3 == userId) {
-                                readyButton3.style.display = 'inline-block';
-                            }
-                            if (lobbys.player4 == userId) {
-                                readyButton4.style.display = 'inline-block';
-                            }
-                            // readyButton.style.display = 'inline-block';
-                            // readyButton.disabled = false;
+                            // if(userId == userId){
+                            //     readyButton1.style.display = 'inline-block';
+                            // }
+                            // if (lobbys.player2 == userId) {
+                            //     readyButton2.style.display = 'inline-block';
+                            // }
+                            // if (lobbys.player3 == userId) {
+                            //     readyButton3.style.display = 'inline-block';
+                            // }
+                            // if (lobbys.player4 == userId) {
+                            //     readyButton4.style.display = 'inline-block';
+                            // }
                         } else {
                             alert(result.message || 'Failed to join the lobby.');
                         }
@@ -138,12 +138,12 @@
                 //     startButton.style.display = 'inline-block';
                 // }
 
-                // li.querySelector('.ready-btn').addEventListener('click', async (event) => {
-                //     readyButton.disabled = true;
-                //     readyButton.innerText = 'Ready (Checked)';
-                //     startButton.style.display = 'inline-block';
-                //     // checkIfAllPlayersReady(lobbyId);
-                // });
+                li.querySelector('.ready-btn').addEventListener('click', async (event) => {
+                    readyButton.disabled = true;
+                    readyButton.innerText = 'Ready (Checked)';
+                    startButton.style.display = 'inline-block';
+                    // checkIfAllPlayersReady(lobbyId);
+                });
 
                 li.querySelector('.start-btn').addEventListener('click', async (event) => {
                     const lobbyId = event.target.getAttribute('data-lobby-id');
